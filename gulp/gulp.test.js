1d222fb3dca1206a50c2df5f6dc3b6af757bc3db
Pierre GILLON <gillon.pierre69@gmail.com>
2015-12-11 23:37:57 +0100

    Update the test tasks to be synchronized with karma execution.

--- gulp/gulp.test.js
+++ gulp/gulp.test.js
@@ -12,63 +12,68 @@
         /**
          * @description Test tasks : Launch unit tests in karma with different modes.
          */
-        gulp.task('test', 'Start a single run of all unit tests.', ['merge-template-files-to-dist'], function () {
-            return runKarmaOnCode({
-                action: 'run'
-            });
+        gulp.task('test', 'Start a single run of all unit tests.', ['merge-template-files-to-dist'], function (done) {
+            runKarmaOnSourceCode({singleRun: true}, done);
         });
-        gulp.task('test-w', 'Start a continuous run of all unit tests.', ['merge-template-files-to-dist'], function () {
-            return runKarmaOnCode({
-                action: 'watch'
-            });
+        gulp.task('test-w', 'Start a continuous run of all unit tests.', ['merge-template-files-to-dist'], function (done) {
+            runKarmaOnSourceCode({singleRun: false}, done);
         });
-        gulp.task('test-c', 'Start a single run of all unit tests and build a coverage summary.', ['merge-template-files-to-dist'], function () {
-            return runKarmaOnCode({
+        gulp.task('test-c', 'Start a single run of all unit tests and build a coverage summary.', ['merge-template-files-to-dist'], function (done) {
+            runKarmaOnSourceCode({
+                singleRun: true,
                 preprocessors: {
                     'app/js/**/*.js': 'coverage'
                 },
-                action: 'run',
                 reporters: ['coverage'],
                 coverageReporter: {
                     type: 'text-summary'
                 }
-            });
+            }, done);
         });
-        gulp.task('test-debug', 'Start a debug session of all unit tests.', ['merge-template-files-to-dist'], function () {
-            return runKarmaOnCode({
-                action: 'watch',
+        gulp.task('test-debug', 'Start a debug session of all unit tests.', ['merge-template-files-to-dist'], function (done) {
+            runKarmaOnSourceCode({
+                singleRun: false,
                 browsers: ['Chrome']
-            });
+            }, done);
         });
-        gulp.task('test-dist', 'Start a single run of all unit tests, based on the full minified built application in the dist folder.', [], function (callback) {
-            runSequence('build-min', 'test-current-dist', callback);
+        gulp.task('test-dist', 'Start a single run of all unit tests, based on the full minified built application in the dist folder.', [], function (done) {
+            runSequence(
+                'build-min',
+                'test-current-dist',
+                done);
         });
 
         // ----- SubTasks
-        gulp.task('test-current-dist', false, [], function () {
-            return runKarmaOnDistCode({
-                action: 'run'
-            });
+        gulp.task('test-current-dist', false, [], function (done) {
+            runKarmaOnDistCode({singleRun: true}, done);
         });
 
         // ----- Utils
-        function runKarmaOnDistCode(configuration) {
+        function runKarmaOnDistCode(configuration, done) {
             var files = utils.bower.getJsLibraries({devDependencies: true, dependencies: true})
                 .concat(parameters.jsTestFiles)
                 .concat(path.join(parameters.distFolderPath, parameters.applicationFileName + '*js'))
                 .concat(path.join(parameters.distFolderPath, parameters.templateFileName + '*js'));
-            return runKarmaOnCode(configuration, files);
+
+            configuration.configFile = path.resolve(parameters.karmaFilePath);
+            configuration.files = files;
+
+            var server = new karma.Server(configuration, function (exitCode) {
+                if (exitCode !== 0) {
+                    done('some failed tests on dist folder.');
+                }
+                else {
+                    done();
+                }
+            });
+            server.start();
         }
 
-        function runKarmaOnCode(configuration, files) {
+        function runKarmaOnSourceCode(configuration, done) {
             configuration.configFile = path.resolve(parameters.karmaFilePath);
-            configuration.singleRun = configuration.action === 'run';
-
-            if (files) {
-                configuration.files = files;
-            }
-
-            var server = new karma.Server(configuration);
+            var server = new karma.Server(configuration, function () {
+                done();
+            });
             server.start();
         }
     }
