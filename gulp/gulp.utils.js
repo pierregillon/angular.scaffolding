ac92fc27efaf489229c921a7946c28735ac4fbf1
Pierre GILLON <gillon.pierre69@gmail.com>
2015-12-11 23:51:28 +0100

    Simplify the bower helper.

--- gulp/gulp.utils.js
+++ gulp/gulp.utils.js
@@ -1,10 +1,6 @@
 (function(module, require){
     'use strict';
 
-    module.exports = {
-        bower : new BowerHelper()
-    };
-
     var gulp = require('gulp-help')(require('gulp')),
         wiredep = require('wiredep');
 
@@ -12,23 +8,15 @@
         var self = this;
 
         self.getJsLibraries = function (wiredepParams) {
-            return getBowerDependencies(wiredep(wiredepParams).js);
+            return wiredep(wiredepParams).js || [];
         };
         self.getCssLibraries = function (wiredepParams) {
-            return getBowerDependencies(wiredep(wiredepParams).css);
+            return wiredep(wiredepParams).css || [];
         };
-
-        function getBowerDependencies(values) {
-            var dependencies = [];
-            if (values) {
-                values.forEach(function (dependency) {
-                    var index = dependency.indexOf('bower_components');
-                    var wellFormattedDependency = dependency.substr(index, dependency.length - index).split("\\").join('/');
-                    dependencies.push(wellFormattedDependency);
-                });
-            }
-            return dependencies;
-        }
     }
+
+    module.exports = {
+        bower : new BowerHelper()
+    };
 
 })(module, require);
