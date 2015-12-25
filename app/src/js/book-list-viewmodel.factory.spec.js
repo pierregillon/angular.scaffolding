adca25fa27d127e465a60a4ea485554a16229c72
Pierre GILLON <gillon.pierre69@gmail.com>
2015-12-25 22:56:47 +0100

    Refactor some unit tests.

--- app/src/js/book-list-viewmodel.factory.spec.js
+++ app/src/js/book-list-viewmodel.factory.spec.js
@@ -5,7 +5,7 @@
 
         var BookListViewModel;
         var bookRepository;
-        var $rootScope; 
+        var $rootScope;
 
         beforeEach(angular.mock.module('main'));
         beforeEach(angular.mock.inject(function (_BookListViewModel_, _bookRepository_, $q, _$rootScope_) {
@@ -20,36 +20,42 @@
             expect(BookListViewModel).to.be.a('function');
         });
 
-        it('should have a load method', function(){
-            var viewModel = new BookListViewModel();
-            expect(viewModel.load).to.be.a('function');
+        describe('[behaviours]', function(){
+            var viewModel;
+
+            beforeEach(angular.mock.inject(function (_bookRepository_, $q, _$rootScope_) {
+                $rootScope = _$rootScope_;
+
+                viewModel = new BookListViewModel();
+
+                bookRepository = _bookRepository_;
+                bookRepository.getBooks = sinon.stub().returns($q.when([]));
+            }));
+
+            it('should have a load method', function(){
+                expect(viewModel.load).to.be.a('function');
+            });
+
+            it('should have an empty collection of books by default.', function(){
+                expect(viewModel.books).to.be.an('array');
+                expect(viewModel.books).to.have.length(0);
+            });
+
+            it('should request first 30 books from repository when loaded.', function(){
+                viewModel.load();
+
+                expect(bookRepository.getBooks).to.have.been.calledWith(0, 30);
+            });
+
+            it('should set book collection from book repository when loaded.', angular.mock.inject(function($q){
+                bookRepository.getBooks.returns($q.when([{}, {}]));
+
+                viewModel.load();
+                $rootScope.$apply();
+
+                expect(viewModel.books).to.have.length(2);
+            }));
         });
-
-        it('should have an empty collection of books by default.', function(){
-            var viewModel = new BookListViewModel();
-
-            expect(viewModel.books).to.be.an('array');
-            expect(viewModel.books).to.have.length(0);
-        });
-
-        it('should request first 30 books from repository when loaded.', function(){
-            var viewModel = new BookListViewModel();
-
-            viewModel.load();
-
-            expect(bookRepository.getBooks).to.have.been.calledWith(0, 30);
-        });
-
-        it('should set book collection from book repository when loaded.', angular.mock.inject(function($q){
-            bookRepository.getBooks.returns($q.when([{}, {}]));
-
-            var viewModel = new BookListViewModel();
-
-            viewModel.load();
-            $rootScope.$apply();
-
-            expect(viewModel.books).to.have.length(2);
-        }));
     });
 
 })();
